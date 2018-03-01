802e96c6d12f712ff81e6c6691049242dc94024d
liuweijw <liuweijw.github@foxmail.com>
2018-03-01 17:52:33 +0800

    登录通过mock模拟数据，同时增加接口服务查询并存方式测试

--- src/views/admin/user/index.vue
+++ src/views/admin/user/index.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
     ---------------admin/user/index------------------
-    <!-- <el-button type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="getMenuList">{{$t('login.logIn')}}</el-button> -->
-
+    <!-- <el-button type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="getMenuList">查询测试</el-button>
+    <br/>
+    <el-input
+      type="textarea"
+      :rows="10"
+      placeholder="请输入内容"
+      v-model="requestResponse">
+    </el-input> -->
   </div>
 </template>
 
 <script>
 import request from '@/utils/request'
 export default {
+  data() {
+    return {
+      requestResponse: ''
+    }
+  },
   methods: {
     getMenuList() {
       request({
         url: '/admin/menu/findMenuByRole/ROLE_ADMIN/',
         method: 'get'
       }).then(res => {
-        console.log(res)
+        this.requestResponse = JSON.stringify(res)
       })
     }
   }
